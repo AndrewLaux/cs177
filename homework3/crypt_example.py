a3a97041195df3c1d6085a2e6125fc944f1babf7
unknown <Laux.m.andrew@gmail.com>
2018-10-24 15:00:48 -0700

    Added words.txt from piazza.

--- homework3/crypt_example.py
+++ homework3/crypt_example.py
@@ -25,7 +25,7 @@ if len(sys.argv) != 3:
     print "Usage: {} password salt".format(sys.argv[0])
 else:
     password = sys.argv[1]
-    salt = "$6$aA349808Abcd$"
+    salt = sys.argv[2]
 
     e_pass = crypt.crypt(password, salt)
     print "Password: {} Salt: {} Hash: {}".format(password, salt, e_pass)
