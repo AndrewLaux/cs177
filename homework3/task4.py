825e88ccc85e0d471c348163ba1c146b224bb522
unknown <Laux.m.andrew@gmail.com>
2018-10-24 14:51:20 -0700

    Versionof progwith hardcoded guesses.

--- homework3/task4.py
+++ homework3/task4.py
@@ -1,11 +1,27 @@
 import sys
 import crypt
+import getpass
+from hmac import compare_digest as compare_hash
 
-if len(sys.argv) != 2:
-    print "Usage: {} password salt".format(sys.argv[0])
-else:
-    password = sys.argv[1]
-    salt = "$6$aBcDeF$qn4wyWpQKwjaKGr02tGUWKcFjl0p90b68.oaaJTFX87UzsSWIzq3ZoAEG0/xUQ1kcYTiHkKqye1Qat6vL4rMZ."
+#Hashes from etc/shadow.txt file.
+jons_hash = "$6$aBcDeF$qn4wyWpQKwjaKGr02tGUWKcFjl0p90b68.oaaJTFX87UzsSWIzq3ZoAEG0/xUQ1kcYTiHkKqye1Qat6vL4rMZ."
+targ_hash = "aa.YVJDT1VruA"
+tryn_hash = "$5$aAbBcCdD$TSZ4rpRmpbVDoas2FK97mHEZwXX"
+arya_hash = "$1$bAc99821$noxC9VXXiMuA0IRfECCVA/"
 
-    e_pass = crypt.crypt(password, salt)
-    print(e_pass)
+#Empty dictionary for found passwords.
+found = {}
+
+#Keeptrackof howmany guesses are made.
+count = 0
+
+#Check password guess, given a known hash.
+def check_pass(guess, hash):
+    print(guess)
+    if compare_hash(crypt.crypt(guess, hash), hash):
+        found[hash] = guess
+        
+check_pass("GameOfThrones", jons_hash)
+check_pass("GameOfThronesRulez", targ_hash)
+
+
