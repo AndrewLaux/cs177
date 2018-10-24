04eea577585f786cb346b219665d647d074371a4
unknown <Laux.m.andrew@gmail.com>
2018-10-24 13:59:00 -0700

    Experimenting with salt value hardcoded.

--- homework3/crypt_example.py
+++ homework3/crypt_example.py
@@ -25,7 +25,7 @@ if len(sys.argv) != 3:
     print "Usage: {} password salt".format(sys.argv[0])
 else:
     password = sys.argv[1]
-    salt = sys.argv[2]
+    salt = "$6$aA349808Abcd$"
 
     e_pass = crypt.crypt(password, salt)
     print "Password: {} Salt: {} Hash: {}".format(password, salt, e_pass)
--- homework3/task4.py
+++ homework3/task4.py
@@ -1,11 +1,11 @@
 import sys
 import crypt
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 2:
     print "Usage: {} password salt".format(sys.argv[0])
 else:
     password = sys.argv[1]
-    salt = sys.argv[2]
+    salt = "$6$aA349808Abcd$"
 
     e_pass = crypt.crypt(password, salt)
     print "Password: {} Salt: {} Hash: {}".format(password, salt, e_pass)
