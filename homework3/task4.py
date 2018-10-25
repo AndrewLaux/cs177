4d1f7a4360ba22e554760e3b5a7b8e4928d19de7
AndrewLaux <Laux.m.andrew@gmail.com>
2018-10-24 18:12:32 -0700

    Added some wordlists to try.

--- homework3/task4.py
+++ homework3/task4.py
@@ -7,6 +7,7 @@ from hmac import compare_digest as compare_hash
 def check_pass(guess, hash):
     if compare_hash(crypt.crypt(guess, hash), hash):
         found[hash] = guess
+        println(guess)
 
 #Hashes from etc/shadow.txt file.
 jons_hash = "$6$aBcDeF$qn4wyWpQKwjaKGr02tGUWKcFjl0p90b68.oaaJTFX87UzsSWIzq3ZoAEG0/xUQ1kcYTiHkKqye1Qat6vL4rMZ."
