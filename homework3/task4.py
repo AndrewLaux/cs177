ef5eba3f860c3327c199d7c25c3542fddedd9ac1
AndrewLaux <Laux.m.andrew@gmail.com>
2018-10-25 00:37:51 -0700

    Added mutatorsto intellegent mode.

--- homework3/task4.py
+++ homework3/task4.py
@@ -31,25 +31,38 @@ mode = sys.argv[2]
 if mode == "intellegent" : 
     with open(filename) as f:
         for line in f:
-            mutations = List()
+            mutations = list()
 
-            mutations[0] = line
+            mutations.append(line)
 
             #Add capital mutation
-            mutations[1] = mutations[0].capitalize()
+            mutations.append(mutations[0].capitalize())
 
             #Add capslock mutation
-            mutations[2] = mutations[0].upper()
+            mutations.append(mutations[0].upper())
 
             #Add numbers 1-10, 420, 66, 22, 99, 55, 69, 13, 777, 666, 
+            for i in range(0,11):
+                mutations.append(mutations[0] + str(i))
+            mutations.append(mutations[0] + str(420))
+            mutations.append(mutations[0] + str(66))
+            mutations.append(mutations[0] + str(22))
+            mutations.append(mutations[0] + str(99))
+            mutations.append(mutations[0] + str(69))
+            mutations.append(mutations[0] + str(13))
+            mutations.append(mutations[0] + str(777))
+            mutations.append(mutations[0] + str(666))
+
+            #Reverse original word
+            mutations.append(mutations[0][::-1])
 
             #Try each mutation
             for word in mutations:
                 count += 1
-                check_pass(i, jons_hash, count)
-                check_pass(i, targ_hash, count)
-                check_pass(i, tryn_hash, count)
-                check_pass(i, arya_hash, count)
+                check_pass(word, jons_hash, count)
+                check_pass(word, targ_hash, count)
+                check_pass(word, tryn_hash, count)
+                check_pass(word, arya_hash, count)
 
 #Dumb parsing of guess file.
 else:
